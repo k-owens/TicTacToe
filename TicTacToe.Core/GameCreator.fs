17bdb4d7fc2721aa77f6352d8f2c146ec217434d
Keith Owens <kowens@pekininsurance.com>
2016-10-04 11:09:42 -0500

    Add more choices for creating a game.

--- TicTacToe.Core/GameCreator.fs
+++ TicTacToe.Core/GameCreator.fs
@@ -7,8 +7,8 @@ module GameCreator =
     let createCurrentGame (currentBoard : char list)=
         [for i in 0 .. currentBoard.Length-1 -> if currentBoard.[i] = '_' then None else Some(currentBoard.[i])]
 
-    let createGame (turnNumber, currentBoard : char list) =
+    let createGame (turnNumber, currentBoard, player1Type, player2Type) =
         let game = {BoardSize = 3; CurrentBoard = createCurrentGame(currentBoard); TurnNumber = turnNumber; IsInverted = true}
-        let player1 = {PlayerType = 1; ComputerAlgorithm = minimaxMove; PlayerCharacter = 'O'} 
-        let player2 = {PlayerType = 1; ComputerAlgorithm = minimaxMove; PlayerCharacter = 'X'}
+        let player1 = {PlayerType = player1Type; ComputerAlgorithm = minimaxMove; PlayerCharacter = 'O'} 
+        let player2 = {PlayerType = player2Type; ComputerAlgorithm = minimaxMove; PlayerCharacter = 'X'}
         {GameBoard = game; Players = [player1;player2]}
