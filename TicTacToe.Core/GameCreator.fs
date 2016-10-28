17b857d88941419b02dcd6cb37937b1b600d8aeb
Keith Owens <kowens@pekininsurance.com>
2016-10-28 09:20:08 -0500

    Change tokens to constants that outer programs cannot change.

--- TicTacToe.Core/GameCreator.fs
+++ TicTacToe.Core/GameCreator.fs
@@ -5,8 +5,8 @@ module GameCreator =
     open MinimaxAlgorithm
 
 
-    let createGame (turnNumber, currentBoard, player1Type, player2Type, player1Character, player2Character) =
+    let createGame (turnNumber, currentBoard, player1Type, player2Type) =
         let game = {BoardSize = 3; CurrentBoard = currentBoard; TurnNumber = turnNumber; IsInverted = true}
-        let player1 = {IsHuman = player1Type; ComputerAlgorithm = minimaxMove; PlayerCharacter = player1Character} 
-        let player2 = {IsHuman = player2Type; ComputerAlgorithm = minimaxMove; PlayerCharacter = player2Character}
+        let player1 = {IsHuman = player1Type; ComputerAlgorithm = minimaxMove; PlayerCharacter = 1} 
+        let player2 = {IsHuman = player2Type; ComputerAlgorithm = minimaxMove; PlayerCharacter = 2}
         {GameBoard = game; Players = [player1;player2]}
